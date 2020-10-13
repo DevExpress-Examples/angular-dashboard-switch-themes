456c271dbca21ed1c94b398b8083861925e4802d
Sergey Nakhankov <sergey.nakhankov@devexpress.com>
2020-10-13 16:55:00 +0300

    Add back end

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 Our HTML JS Dashboard does not provide a way to switch themes in Angular applications using the same mechanism as in our [online demo](https://demos.devexpress.com/Dashboard/). In the case of an Angular application, it is necessary to implement this mechanism manually. This example demonstrates a way to implement the required functionality.
 
-For more information about the implementation, please refer to our[ HTML JS Dashboard - How to switch themes/color schemes in Angular applications](https://www.devexpress.com/Support/Center/Question/Details/T741077/html-js-dashboard-how-to-switch-themes-color-schemes-in-angular-applications) KB article.  
+For more information about the implementation, please refer to our[ HTML JS Dashboard - How to switch themes/color schemes in Angular applications](https://www.devexpress.com/Support/Center/Question/Details/T741077/html-js-dashboard-how-to-switch-themes-color-schemes-in-angular-applications) KB article.
+
+## Run the Project
+
+In the **asp-net-core-dashboard-backend** folder run the following command:
+
+```
+dotnet run
+```
+
+In the **dashboard-angular-app** folder, run the following commands:
+
+```
+npm i
+ng serve --open
+```
---
+++ asp-net-core-dashboard-backend/SessionDashboardStorage.cs
@@ -0,0 +1,70 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace AspNetCoreDashboardCustomPropertiesSample {
+    public class SessionDashboardStorage : DashboardStorageBase {
+        const string DashboardStorageKey = "74cba564-c821-439c-a714-40ff6027b1eb";
+
+        readonly IHttpContextAccessor сontextAccessor;
+
+        protected HttpContext HttpContext { get { return сontextAccessor?.HttpContext; } }
+
+        public SessionDashboardStorage(IHttpContextAccessor contextAccessor) : base() {
+            сontextAccessor = contextAccessor;
+        }
+
+        private Dictionary<string, string> ReadFromSession() {
+            Dictionary<string, string> result = null;
+            ISession session = HttpContext?.Session;
+            if(session != null) {
+                string serializedStorage = session.GetString(DashboardStorageKey) ?? string.Empty;
+                result = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedStorage);
+                if(result == null) {
+                    result = Initialize();
+                    SaveToSession(result);
+                }
+            }
+            return result;
+        }
+        private void SaveToSession(Dictionary<string, string> storage) {
+            HttpContext?.Session?.SetString(DashboardStorageKey, JsonConvert.SerializeObject(storage));
+        }
+        private Dictionary<string, string> Initialize() {
+            Dictionary<string, string> storage = new Dictionary<string, string>();
+            foreach(string file in Directory.EnumerateFiles((string)AppDomain.CurrentDomain.GetData("DataDirectory"), "Dashboards/*.xml")) {
+                InitializeCore(Path.GetFileNameWithoutExtension(file), storage);
+            }
+            return storage;
+        }
+        private void InitializeCore(string dashboardID, Dictionary<string, string> storage) {
+            string dataDirectoryPath = (string)AppDomain.CurrentDomain.GetData("DataDirectory");
+            string filePath = Path.Combine(dataDirectoryPath, "Dashboards", $"{dashboardID}.xml");
+            if(!storage.ContainsKey(dashboardID) && File.Exists(filePath)) {
+                using(StreamReader reader = new StreamReader(filePath)) {
+                    storage.Add(dashboardID, reader.ReadToEnd());
+                }
+            }
+        }
+
+        protected override IEnumerable<string> GetAvailableDashboardsID() {
+            return ReadFromSession().Keys;
+        }
+        protected override XDocument LoadDashboard(string dashboardID) {
+            Dictionary<string, string> storage = ReadFromSession();
+            return XDocument.Parse(storage[dashboardID]);
+        }
+        protected override void SaveDashboard(string dashboardID, XDocument dashboard, bool createNew) {
+            Dictionary<string, string> storage = ReadFromSession();
+            if(createNew ^ storage.ContainsKey(dashboardID)) {
+                storage[dashboardID] = dashboard.Document.ToString();
+                SaveToSession(storage);
+            }
+        }
+    }
+}
