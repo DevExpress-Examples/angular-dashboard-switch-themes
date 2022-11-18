199a495814e3a1f7f9b642586b4f9ce712399ce9
Polina Tyureva <polina.tyureva@devexpress.com>
2022-11-19 01:42:35 +0400

    update backend

--- asp-net-core-server/DefaultDashboardController.cs
+++ asp-net-core-server/DefaultDashboardController.cs
@@ -2,9 +2,9 @@
 using DevExpress.DashboardWeb;
 using Microsoft.AspNetCore.DataProtection;
 
-namespace AspNetCoreDashboardCustomPropertiesSample {
+namespace AspNetCoreDashboardBackend {
     public class DefaultDashboardController : DashboardController {
-        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider dataProtectionProvider = null)
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider? dataProtectionProvider)
             : base(configurator, dataProtectionProvider) {
         }
     }
