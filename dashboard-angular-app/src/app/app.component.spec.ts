a95f8bacb2ab536c0fab3c012456b5cc6d29dac0
Polina Tyureva <polina.tyureva@devexpress.com>
2024-01-05 16:07:17 +0400

    Update to Angular 17

--- dashboard-angular-app/src/app/app.component.spec.ts
+++ dashboard-angular-app/src/app/app.component.spec.ts
@@ -1,35 +1,29 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard-test'`, () => {
+  it(`should have the 'dashboard-angular-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dashboard-test');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('dashboard-angular-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dashboard-test!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dashboard-angular-app');
   });
 });
--- dashboard-angular-app/src/app/app.component.ts
+++ dashboard-angular-app/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component, AfterViewInit, ElementRef, OnDestroy, Inject } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, OnDestroy, Inject  } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { DxDashboardControlModule } from 'devexpress-dashboard-angular';
 import { DashboardControl, ResourceManager, DashboardControlArgs } from 'devexpress-dashboard';
-import { DOCUMENT } from '@angular/common';
 import themes from "devextreme/ui/themes";
 
 declare var require: (e) => any;
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, DxDashboardControlModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
+
 export class AppComponent implements AfterViewInit {
   private dashboardControl: DashboardControl;
   private colorSchemeIcon = '<svg id="colorSchemeIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.dx_gray{fill:#7b7b7b;}</style></defs><title>Themes copy</title><path class="dx_gray" d="M12,3a9,9,0,0,0,0,18c7,0,1.35-3.13,3-5,1.4-1.59,6,4,6-4A9,9,0,0,0,12,3ZM5,10a2,2,0,1,1,2,2A2,2,0,0,1,5,10Zm3,7a2,2,0,1,1,2-2A2,2,0,0,1,8,17Zm3-8a2,2,0,1,1,2-2A2,2,0,0,1,11,9Zm5,1a2,2,0,1,1,2-2A2,2,0,0,1,16,10Z" /></svg>';
